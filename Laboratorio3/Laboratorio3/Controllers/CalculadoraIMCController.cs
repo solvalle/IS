704fa447a4c6e4dbfc9131e5637dd297e551994a
solvalle <solvalle@github.com>
2021-09-06 03:03:30 -0600

    Laboratorio 3 terminado

---
+++ Laboratorio3/Laboratorio3/Controllers/CalculadoraIMCController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Laboratorio3.Models;
+using System;
+
+namespace Laboratorio3.Controllers
+{
+    public class CalculadoraIMCController : Controller
+    {
+        public ActionResult ResultadoIMC()
+        {
+            PersonaModel persona = new PersonaModel(1, "Cristiano Ronaldo", 84.0, 1.87);
+            double IMC = persona.Peso / (persona.Estatura * persona.Estatura);
+            ViewBag.IMC = IMC;
+            ViewBag.persona = persona;
+            return View();
+        }
+
+        public ActionResult ResultadosAleatoriosIMC()
+        {
+            PersonaModel[] personas = new PersonaModel[30];
+            double[] IMC = new double[30];
+            Random random = new Random();
+            double peso = 0.0, estatura = 0.0;
+
+            for (int i = 0; i < 30; ++i)
+            {
+                peso = random.NextDouble() * (150.0 - 20.0) + 20.0;
+                estatura = random.NextDouble() * (2.0 - 1.0) + 1.0;
+                personas[i] = new PersonaModel(i+1, "Sin nombre", peso, estatura);
+                IMC[i] = personas[i].Peso / (personas[i].Estatura * personas[i].Estatura);               
+            }
+            ViewBag.IMC = IMC;
+            ViewBag.personas = personas;
+            return View();
+        }
+    }
+
+}
---
+++ Laboratorio3/Laboratorio3/Models/PersonaModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio3.Models
+{
+    public class PersonaModel
+    {
+        // Propiedades: cuando se instancia una persona se puede hacer. Peso=valor
+        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public double Peso { get; set; }
+        public double Estatura { get; set; }
+        public PersonaModel()
+        {
+            this.Nombre = "Sin nombre";
+            this.Id = 0;
+            this.Peso = 0.0;
+            this.Estatura = 0.0;
+        }
+
+        public PersonaModel(int Id, string Nombre, double Peso, double Estatura)
+        {
+            this.Nombre = Nombre;
+            this.Id = Id;
+            this.Peso = Peso;
+            this.Estatura = Estatura;
+
+        }
+    }
+}
